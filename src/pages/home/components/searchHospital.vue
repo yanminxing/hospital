b22352832917ceabfe21ab5949473f75f0acb339
blizx <1194474469@qq.com>
2023-09-02 16:15:42 +0800

    feat:首页主要静态

---
+++ src/pages/home/components/searchHospital.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="page-home-hospital">
+		<div class="page-home-hospital-name">
+			医院
+		</div>
+		<div class="page-home-hospital-content">
+			<span>等级:</span>
+			<div class="page-home-hospital-content-right">
+				<span v-for="item in 20" >全部医院</span>
+			</div>
+		</div>
+		<div class="page-home-hospital-content">
+			<span>地区:</span>
+			<div class="page-home-hospital-content-right">
+				<span v-for="item in 20" >全部地区</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/common.scss";
+@import '@/style/elementReset.scss';
+
+.page-home-hospital {
+
+	&-name{
+		font-weight: 600;
+		font-size: 20px;
+	}
+	&-content{
+		display: flex;
+		margin-top: 10px;
+		color: #bbbbbb;
+
+		& :first-child {
+			line-height: 26px;
+		}
+		&-right{
+			flex:1;
+			margin-left: 10px;
+
+			span {
+				margin-right: 10px;
+				line-height: 26px;
+			}
+		}
+	}
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,8 +6,32 @@
 		<div class="page-home-search align-center-center">
 			<KeySearch style="width: 600px" placeholder="请输入医院" defaultValue="有值"/>
 			<el-button type="primary" :icon="Search">搜 索</el-button>
-			<!--<el-input v-model="form.key" clearable></el-input>-->
 		</div>
+		<el-row :gutter="20" class="content">
+			<el-col :span="19">
+				<!--医院搜索条件-->
+				<SearchHospital></SearchHospital>
+				<el-row :gutter="20">
+					<el-col :span="12" v-for="item in 8" :key="item">
+						<MainContent></MainContent>
+					</el-col>
+				</el-row>
+			</el-col>
+			<el-col :span="5">2</el-col>
+		</el-row>
+		<el-pagination
+			v-model:current-page="currentPage4"
+			v-model:page-size="pageSize4"
+			class="page-home-pagination"
+			:page-sizes="[100, 200, 300, 400]"
+			:small="small"
+			:disabled="disabled"
+			:background="background"
+			layout="prev, pager, next, jumper, total, sizes,"
+			:total="400"
+			@size-change="handleSizeChange"
+			@current-change="handleCurrentChange"
+		/>
 	</div>
 </template>
 
@@ -16,10 +40,12 @@ import {ref} from 'vue';
 import CarouselComp from './components/carouselComp.vue';
 import {Search} from '@element-plus/icons-vue';
 import KeySearch from '@/components/autoSearch/AutoSearch.vue';
+import SearchHospital from './components/searchHospital.vue';
+import MainContent from './components/mainContent.vue';
 
 const form = ref({
 	key: ''
-})
+});
 
 console.log(getComputedStyle(window.document.documentElement)['fontSize']);
 
@@ -37,5 +63,9 @@ console.log(getComputedStyle(window.document.documentElement)['fontSize']);
 			margin-right: 10px;
 		}
 	}
+}
+
+.content {
+	padding: 20px;
 }
 </style>
